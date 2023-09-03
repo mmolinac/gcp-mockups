03e644f8cab69ed931cf696356c536a4e05c9cf2
Manuel Molina Cuberos <mmc@pocosmhz.org>
2023-09-03 20:13:34 +0200

    on-premises-k8s-cluster: Startup script. First version.

---
+++ on-premises-k8s-cluster/startup_script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Starting software installation" | sudo tee /tmp/startup.log
+echo "Installing packages as user `whoami`" | sudo tee -a /tmp/startup.log
+apt update
+apt-get install -y apt-transport-https ca-certificates curl
+# Download the public signing key  for the Kubernetes package repositories
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# Add the appropriate Kubernetes apt repository
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
