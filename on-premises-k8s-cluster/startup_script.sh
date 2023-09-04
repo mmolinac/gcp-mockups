a57cb9eff22387ef36456f7b642d07254172f915
Manuel Molina Cuberos <mmc@pocosmhz.org>
2023-09-04 03:14:05 +0200

    on-premises-k8s-cluster: Added more Kubernetes software installation steps for each node.

--- on-premises-k8s-cluster/startup_script.sh
+++ on-premises-k8s-cluster/startup_script.sh
@@ -1,12 +1,64 @@
 #!/bin/sh
-echo "Starting software installation" | sudo tee /tmp/startup.log
-echo "Installing packages as user `whoami`" | sudo tee -a /tmp/startup.log
+export DEBIAN_FRONTEND=noninteractive
+echo "Starting software installation" | tee /tmp/startup.log
+echo "Installing packages as user `whoami`" | tee -a /tmp/startup.log
+apt-get update
+apt install -y jq
+
+NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
+
+# Add modules to load for Kubernetes
+# Install modules and let iptables see bridged traffic
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+(lsmod | grep overlay) || modprobe overlay
+(lsmod | grep br_netfilter) || modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sysctl --system
+
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
+# Install packages
 apt update
-apt-get install -y apt-transport-https ca-certificates curl
+apt-get install -y apt-transport-https ca-certificates curl gnupg
 # Download the public signing key  for the Kubernetes package repositories
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | \
+  gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 # Add the appropriate Kubernetes apt repository
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | \
+  tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubectl
+apt-get install -y kubelet kubeadm kubectl
+# Install Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | \
+  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt-get install -y docker-ce docker-ce-cli containerd.io \
+  docker-buildx-plugin docker-compose-plugin netcat-openbsd
+
+# Create configuration files
+mkdir -p /etc/containerd
+test -f /etc/containerd/config.toml || (containerd config default | \
+  tee /etc/containerd/config.toml && systemctl restart containerd)
+
